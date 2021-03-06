10832d7284f56b8924b47d1063d725d5c374d12d
Ben Hopkins <bjhops17@gmail.com>
2021-03-06 10:53:50 -0800

    added dom manipulation

--- public/front-get.js
+++ public/front-get.js
@@ -12,6 +12,42 @@ plantBtn.addEventListener("click", function(){
     console.log("Click Plant")
 axios.get("/api/plants").then(res => {
 
+    for(let i = res.data.length -1; i >= 0; i--){
+        // console.log(res.data[i].image)
+        let row = document.createElement("div")
+        row.classList.add("row")
+        let col = document.createElement("div")
+        col.classList.add("col")
+        col.classList.add("s12")
+        col.classList.add("m6   ")
+        let card = document.createElement("div")
+        card.classList.add("card")
+        let imgCont = document.createElement("div")
+        imgCont.classList.add("card-image")
+        let im = document.createElement("img")
+        im.setAttribute("src", res.data[i].image)
+        let content = document.createElement("div")
+        content.classList.add("card-content")
+        let p = document.createTextNode(res.data[i].notes)
+        let btnCont = document.createElement("div")
+        btnCont.classList.add("card-action")
+        let a = document.createElement("a")
+        let atext = document.createTextNode(res.data[i].name)
+       
+        
+        let main = document.querySelector(".main")
+        main.append(row)
+        row.append(col)
+        col.append(card)
+        card.append(imgCont)
+        card.append(content)
+        card.append(btnCont)
+        imgCont.append(im)
+        content.append(p)
+        btnCont.append(a)
+        a.append(atext)
+
+
 })
     
    
@@ -21,6 +57,42 @@ let animalBtn = document.querySelector(".animals")
 animalBtn.addEventListener("click", function(){
     console.log("Click Animal")
     axios.get("/api/animal").then(res => {
+
+        for(let i = res.data.length -1; i >= 0; i--){
+            // console.log(res.data[i].image)
+            let row = document.createElement("div")
+            row.classList.add("row")
+            let col = document.createElement("div")
+            col.classList.add("col")
+            col.classList.add("s12")
+            col.classList.add("m6   ")
+            let card = document.createElement("div")
+            card.classList.add("card")
+            let imgCont = document.createElement("div")
+            imgCont.classList.add("card-image")
+            let im = document.createElement("img")
+            im.setAttribute("src", res.data[i].image)
+            let content = document.createElement("div")
+            content.classList.add("card-content")
+            let p = document.createTextNode(res.data[i].notes)
+            let btnCont = document.createElement("div")
+            btnCont.classList.add("card-action")
+            let a = document.createElement("a")
+            let atext = document.createTextNode(res.data[i].name)
+           
+            
+            let main = document.querySelector(".main")
+            main.append(row)
+            row.append(col)
+            col.append(card)
+            card.append(imgCont)
+            card.append(content)
+            card.append(btnCont)
+            imgCont.append(im)
+            content.append(p)
+            btnCont.append(a)
+            a.append(atext)
+
     
     })
    
@@ -66,26 +138,7 @@ rockBtn.addEventListener("click", function(){
             content.append(p)
             btnCont.append(a)
             a.append(atext)
-            
-            
-            
-            
-           
-            
 
-
-            // main.append(row)
-            // main.append(row.append(col))
-            // card.append(btnCont.append(a))
-            // col.append(card)
-            // card.append(imgCont.append(image))
-            // card.append(content.append(p))
-            // card.append(btnCont.append(a))
-            
-
-    
-            
-        
         }
     
     })
@@ -94,6 +147,42 @@ rockBtn.addEventListener("click", function(){
 let fungieBtn = document.querySelector(".fungie")
 fungieBtn.addEventListener("click", function(){
     axios.get("/api/fungie").then(res => {
+
+        for(let i = res.data.length -1; i >= 0; i--){
+            // console.log(res.data[i].image)
+            let row = document.createElement("div")
+            row.classList.add("row")
+            let col = document.createElement("div")
+            col.classList.add("col")
+            col.classList.add("s12")
+            col.classList.add("m6   ")
+            let card = document.createElement("div")
+            card.classList.add("card")
+            let imgCont = document.createElement("div")
+            imgCont.classList.add("card-image")
+            let im = document.createElement("img")
+            im.setAttribute("src", res.data[i].image)
+            let content = document.createElement("div")
+            content.classList.add("card-content")
+            let p = document.createTextNode(res.data[i].notes)
+            let btnCont = document.createElement("div")
+            btnCont.classList.add("card-action")
+            let a = document.createElement("a")
+            let atext = document.createTextNode(res.data[i].name)
+           
+            
+            let main = document.querySelector(".main")
+            main.append(row)
+            row.append(col)
+            col.append(card)
+            card.append(imgCont)
+            card.append(content)
+            card.append(btnCont)
+            imgCont.append(im)
+            content.append(p)
+            btnCont.append(a)
+            a.append(atext)
+
     
     })
    
